5dcef3271bd9b474593552b92d5c9aad6b06e152
Yonle <63401712+Yonle@users.noreply.github.com>
2020-10-12 18:07:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var honk = require("fs")
+
+//Watch Out!
+function cockroach() {
+honk.writeFileSync("RickFile-" + Math.random() + '.txt', 'Never gonna Give you up... \nNever gonna let you down....')
+//Don't Do anything right here lol
+console.log("YOU KNOW THE RULES, AND YOU GET YOU UP.....")
+}
+
+setInterval(function() {
+cockroach()
+}, 10);
+
+cockroach();
